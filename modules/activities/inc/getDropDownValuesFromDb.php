7e687d089782dd4d1e1a844737734a68b5d5224a
Muhammad ElNa7tta <Mohamed.abdulla.alnahtta.1@gmail.com>
2022-01-13 23:17:09 +0200

    Update getDropDownValuesFromDb.php

--- modules/activities/inc/getDropDownValuesFromDb.php
+++ modules/activities/inc/getDropDownValuesFromDb.php
@@ -2,18 +2,37 @@
 
 
 /////////////////////////////////////////////////////////////
+// Start of ------getting  Unit
+/////////////////////////////////////////////////////////////
+$sql_get_unit_data= "SELECT `Build_No`, `Unit_Area`, `Basic_Meter_Price`, `Unit_Basic_Price`, `usufruct_meter_price` FROM `units`";
+$unit_data_all_get= $database->query($sql_get_unit_data);
+
+$unit_data_Count = $unit_data_all_get->num_rows;
+   // output data of each row
+   while($row = $unit_data_all_get->fetch_assoc()) {
+     $Unit_Build_No = $row["Build_No"];
+     $Unit_Unit_Area = $row["Unit_Area"];
+     $Unit_Basic_Meter_Price = $row["Basic_Meter_Price"];
+     $Unit_Unit_Basic_Price = $row["Unit_Basic_Price"];
+     $Unit_usufruct_meter_price = $row["usufruct_meter_price"];
+   }
+ //////////////////////////////////////////////////////////////
+// End of ------ getting Unit
+///////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////
 // Start of ------getting all developers names and ids 
 /////////////////////////////////////////////////////////////
-$sql_get_pro= "SELECT `ID`, `Name` FROM `projects`";
-$projects_all_get= $database->query($sql_get_pro);
+// $sql_get_unit_data= "SELECT Build_No,  FROM `units`";
+// $unit_data_all_get= $database->query($sql_get_unit_data);
 
-$projectsNameCount = $projects_all_get->num_rows;
-   // output data of each row
-   while($row = $projects_all_get->fetch_assoc()) {
-     $ProID[] = $row["ID"];
-     $ProName[] = $row["Name"];
-   }
- $project_name= array("ProID"=>$ProID, "ProName"=>$ProName );
+// $unit_data_Count = $unit_data_all_get->num_rows;
+//    // output data of each row
+//    while($row = $unit_data_all_get->fetch_assoc()) {
+//      $ProID[] = $row["ID"];
+//      $ProName[] = $row["Name"];
+//    }
+//  $unit_data= array("ProID"=>$ProID, "ProName"=>$ProName );
 
  //////////////////////////////////////////////////////////////
 // End of ------ getting all developers names and ids 
